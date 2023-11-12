05074c21c27bbb1d1c0ecb6bf4b9d9769385bdb0
Razzmatazz146 <le1935607@crc-lennox.qc.ca>
2023-11-11 19:06:38 -0500

    Removed unused imports

--- src/main/java/com/champlain/oop2/oop2assignment4/EnclosureListController.java
+++ src/main/java/com/champlain/oop2/oop2assignment4/EnclosureListController.java
@@ -1,22 +1,17 @@
 package com.champlain.oop2.oop2assignment4;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 // TODO: ListView initialization with the default enclosures (Lion, Cougar, Tiger)
